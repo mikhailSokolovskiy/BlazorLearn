088cd07c9abd6cf0612be80fcaa1a25895c905ee
mikhailSokolovskiy <mikhail.sokolovskiy.2002@mail.ru>
2025-06-17 16:06:32 +0500

    refactor pizza configuration dialog and integrate order state management

--- BlazingPizza/Model/Order.cs
+++ BlazingPizza/Model/Order.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using BlazingPizza.Model;
-
-namespace BlazingPizza
+﻿namespace BlazingPizza.Model
 {
     public class Order
     {
